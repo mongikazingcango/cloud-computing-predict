ba3e813c0591b5ba7d9fbde26de2180563371507
mongikazingcango <mongikazingcango@gmail.com>
2023-11-06 19:53:59 +0200

    Updated url field with my api url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://ug5wuzo435.execute-api.eu-west-2.amazonaws.com/default/mongikazi-portfoilo",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
